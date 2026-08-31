5b8737bf208032679db06178b3b60b3e7573b4c4
ngo779998-a11y <ngo779998@gmail.com>
2026-08-31 20:37:42 +0700

    1

--- dynova-sport/app/(client)/shop/page.jsx
+++ dynova-sport/app/(client)/shop/page.jsx
@@ -271,13 +271,12 @@ export default function ShopPage() {
 
       try {
         const [productResponse, categoryResponse] = await Promise.all([
-<<<<<<< HEAD
+
 
           getProducts({ per_page: 400 }),
           getProducts({ per_page: 500 }),
-=======
+
           getProducts({ per_page: 400 }),
->>>>>>> main
           getCategories(),
         ]);
 
